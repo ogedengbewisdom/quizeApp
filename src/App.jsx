c33672b357a940f6a805db3bf68e4cac43499d34
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-11-13 21:48:27 +0100

    root app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
